b08935aa52c4e3b171a2a9fbed42b7e542c65ee4
Adrian Celaya <acelaya0@gmail.com>
2021-07-05 15:57:22 -0500

    Update losses.py

--- mist/losses/losses.py
+++ mist/losses/losses.py
@@ -24,15 +24,3 @@ def dice_loss_weighted(y_true, y_pred):
     den = K.sum(K.square(y_true), axis = axes) + K.sum(K.square(y_pred), axis = axes) + smooth
 
     return K.sum(Wk * num, axis = -1) / K.sum(Wk * den, axis = -1)
-
-# L2 Dice loss
-def dice_loss_soft(y_true, y_pred):
-    smooth = 0.0000001
-
-    # (batch size, depth, height, width, channels)
-    # skip the batch and class axis for calculating Dice score
-    axes = tuple(range(1, len(y_true.shape) - 1))
-    num = 2 * K.sum(y_true * y_pred, axis = axes) + smooth
-    den = K.sum(K.square(y_true), axis = axes) + K.sum(K.square(y_pred), axis = axes) + smooth
-
-    return 1 - K.mean(num/den, axis = -1)
