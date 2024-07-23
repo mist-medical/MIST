f7e7a535ec62103aed2185e0db0ca5a4c0334112
Brayden Mi <braykzmi@gmail.com>
2024-07-23 13:16:27 -0500

    updated to use existing DiceLoss implementation

--- mist/runtime/loss.py
+++ mist/runtime/loss.py
@@ -31,10 +31,9 @@ class DiceLoss(nn.Module):
         loss = torch.mean(loss)
         return loss
 
-#Implement clDice:
-class soft_cldice(nn.Module):
-    def __init__(self, iter_=3, smooth = 1., exclude_background=False):
-        super(soft_cldice, self).__init__()
+class SoftCLDice(nn.Module):
+    def __init__(self, iter_=3, smooth=1., exclude_background=False):
+        super(SoftCLDice, self).__init__()
         self.iter = iter_
         self.smooth = smooth
         self.soft_skeletonize = SoftSkeletonize(num_iter=10)
@@ -46,27 +45,33 @@ class soft_cldice(nn.Module):
             y_pred = y_pred[:, 1:, :, :]
         skel_pred = self.soft_skeletonize(y_pred)
         skel_true = self.soft_skeletonize(y_true)
-        tprec = (torch.sum(torch.multiply(skel_pred, y_true))+self.smooth)/(torch.sum(skel_pred)+self.smooth)    
-        tsens = (torch.sum(torch.multiply(skel_true, y_pred))+self.smooth)/(torch.sum(skel_true)+self.smooth)    
-        cl_dice = 1.- 2.0*(tprec*tsens)/(tprec+tsens)
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true)) + self.smooth) / (torch.sum(skel_pred) + self.smooth)
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred)) + self.smooth) / (torch.sum(skel_true) + self.smooth)
+        cl_dice = 1. - 2.0 * (tprec * tsens) / (tprec + tsens)
         return cl_dice
 
 
-def soft_dice(y_true, y_pred):
-    """[function to compute dice loss]
+class SoftDiceCLDice(nn.Module):
+    def __init__(self, iter_=3, alpha=0.5, smooth=1., exclude_background=False):
+        super(SoftDiceCLDice, self).__init__()
+        self.iter = iter_
+        self.smooth = smooth
+        self.alpha = alpha
+        self.dice_loss = DiceLoss()
+        self.soft_skeletonize = SoftSkeletonize(num_iter=10)
+        self.exclude_background = exclude_background
 
-    Args:
-        y_true ([float32]): [ground truth image]
-        y_pred ([float32]): [predicted image]
-
-    Returns:
-        [float32]: [loss value]
-    """
-    smooth = 1
-    intersection = torch.sum((y_true * y_pred))
-    coeff = (2. *  intersection + smooth) / (torch.sum(y_true) + torch.sum(y_pred) + smooth)
-    return (1. - coeff)
-
+    def forward(self, y_true, y_pred):
+        if self.exclude_background:
+            y_true = y_true[:, 1:, :, :]
+            y_pred = y_pred[:, 1:, :, :]
+        dice = self.dice_loss(y_true, y_pred)
+        skel_pred = self.soft_skeletonize(y_pred)
+        skel_true = self.soft_skeletonize(y_true)
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true)) + self.smooth) / (torch.sum(skel_pred) + self.smooth)
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred)) + self.smooth) / (torch.sum(skel_true) + self.smooth)
+        cl_dice = 1. - 2.0 * (tprec * tsens) / (tprec + tsens)
+        return (1.0 - self.alpha) * dice + self.alpha * cl_dice
 
 class soft_dice_cldice(nn.Module):
     def __init__(self, iter_=3, alpha=0.5, smooth = 1., exclude_background=False):
