Bug fix for cldice and add dynamic weighting

mist/runtime/loss.py
@@ -1,16 +1,7 @@
 import torch
 import torch.nn as nn
-from torch.nn.functional import softmax, one_hot
-from mist.runtime.loss_utils import SoftSkeletonize
-
-
-def get_one_hot(y_true, n_classes):
-    y_true = y_true.to(torch.int64)
-    y_true = one_hot(y_true, num_classes=n_classes)
-    y_true = torch.transpose(y_true, dim0=5, dim1=1)
-    y_true = torch.squeeze(y_true, dim=5)
-    y_true = y_true.to(torch.int8)
-    return y_true
+from torch.nn.functional import softmax
+from mist.runtime.loss_utils import get_one_hot, SoftSkeletonize
 
 
 class DiceLoss(nn.Module):
@@ -31,6 +22,25 @@ class DiceLoss(nn.Module):
         loss = torch.mean(num / den, axis=1)
         loss = torch.mean(loss)
         return loss
+    
+
+class DiceCELoss(nn.Module):
+    def __init__(self):
+        super(DiceCELoss, self).__init__()
+        self.cross_entropy = torch.nn.CrossEntropyLoss()
+        self.dice_loss = DiceLoss()
+
+    def forward(self, y_true, y_pred):
+        # Dice loss
+        loss_dice = self.dice_loss(y_true, y_pred)
+
+        # Prepare inputs
+        y_true = get_one_hot(y_true, y_pred.shape[1]).to(torch.float)
+
+        # Cross entropy loss
+        loss_ce = self.cross_entropy(y_pred, y_true)
+
+        return 0.5*(loss_ce + loss_dice)
 
 
 class SoftCLDice(nn.Module):
@@ -43,7 +53,7 @@ class SoftCLDice(nn.Module):
 
     def forward(self, y_true, y_pred):
         # Prepare inputs
-        y_true = get_one_hot(y_true, y_pred.shape[1])
+        y_true = get_one_hot(y_true, y_pred.shape[1]).to(torch.float)
         y_pred = softmax(y_pred, dim=1)
 
         if self.exclude_background:
@@ -59,38 +69,18 @@ class SoftCLDice(nn.Module):
 
 
 class SoftDiceCLDice(nn.Module):
-    def __init__(self, iterations=3, alpha=0.5, smooth=1., exclude_background=False):
+    def __init__(self, iterations=3, smooth=1., exclude_background=False):
         super(SoftDiceCLDice, self).__init__()
         self.iterations = iterations
         self.smooth = smooth
-        self.alpha = alpha
-        self.dice_loss = DiceLoss()
+        self.region_loss = DiceCELoss()
         self.exclude_background = exclude_background
         self.cldice_loss = SoftCLDice(self.iterations, self.smooth, self.exclude_background)
 
-    def forward(self, y_true, y_pred):
-        dice = self.dice_loss(y_true, y_pred)
+    def forward(self, y_true, y_pred, alpha):
+        region_loss = self.region_loss(y_true, y_pred)
         cldice = self.cldice_loss(y_true, y_pred)
-        return (1.0 - self.alpha) * dice + self.alpha * cldice
-
-
-class DiceCELoss(nn.Module):
-    def __init__(self):
-        super(DiceCELoss, self).__init__()
-        self.cross_entropy = torch.nn.CrossEntropyLoss()
-        self.dice_loss = DiceLoss()
-
-    def forward(self, y_true, y_pred):
-        # Dice loss
-        loss_dice = self.dice_loss(y_true, y_pred)
-
-        # Prepare inputs
-        y_true = get_one_hot(y_true, y_pred.shape[1]).to(torch.float32)
-
-        # Cross entropy loss
-        loss_ce = self.cross_entropy(y_pred, y_true)
-
-        return 0.5*(loss_ce + loss_dice)
+        return alpha * region_loss + (1. - alpha) * cldice
 
 
 class WeightedDiceLoss(nn.Module):

mist/runtime/loss_utils.py
@@ -1,6 +1,16 @@
+"""Loss function utilities."""
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+
+def get_one_hot(y_true, n_classes):
+    y_true = y_true.to(torch.int64)
+    y_true = F.one_hot(y_true, num_classes=n_classes)
+    y_true = torch.transpose(y_true, dim0=5, dim1=1)
+    y_true = torch.squeeze(y_true, dim=5)
+    y_true = y_true.to(torch.int8)
+    return y_true
 
 
 class SoftSkeletonize(nn.Module):
@@ -11,24 +21,24 @@ class SoftSkeletonize(nn.Module):
 
     def soft_erode(self, img):
         # Ensure 5D input for 3D img
-        if len(img.shape) == 5:
-            # Apply maxpooling
-            p1 = -F.max_pool3d(-img, (3,1,1), (1,1,1), (1,0,0))
-            p2 = -F.max_pool3d(-img, (1,3,1), (1,1,1), (0,1,0))
-            p3 = -F.max_pool3d(-img, (1,1,3), (1,1,1), (0,0,1))
-            
-            # Return the min of the pools
-            return torch.min(torch.min(p1, p2), p3)
-        else:
+        if len(img.shape) != 5:
             raise ValueError("[Loss Error] len(img.shape) is not equal to 5")
+            
+        # Apply maxpooling
+        p1 = -F.max_pool3d(-img, (3,1,1), (1,1,1), (1,0,0))
+        p2 = -F.max_pool3d(-img, (1,3,1), (1,1,1), (0,1,0))
+        p3 = -F.max_pool3d(-img, (1,1,3), (1,1,1), (0,0,1))
 
+        # Return the min of the pools
+        return torch.min(torch.min(p1, p2), p3)
+            
     def soft_dilate(self, img):
         # Ensure 5D input for 3D img
-        if len(img.shape) == 5:
-            # Apply max pooling with 3x3x3 kernel size
-            return F.max_pool3d(img, (3,3,3), (1,1,1), (1,1,1))
-        else:
+        if len(img.shape) != 5:
             raise ValueError("[Loss Error] len(img.shape) is not equal to 5")
+            
+        # Apply max pooling with 3x3x3 kernel size
+        return F.max_pool3d(img, (3,3,3), (1,1,1), (1,1,1))
 
     def soft_open(self, img):
         # Soft erode followed by soft dilate

mist/runtime/run.py
@@ -31,7 +31,7 @@ from mist.data_loading.dali_loader import (
 )
 
 from mist.models.get_model import get_model, load_model_from_config, configure_pretrained_model
-from mist.runtime.loss import get_loss, DiceLoss, VAELoss
+from mist.runtime.loss import get_loss, DiceCELoss, VAELoss
 from mist.inference.main_inference import predict_single_example
 
 from mist.runtime.utils import (
@@ -102,6 +102,7 @@ class Trainer:
         else:
             self.class_weights = None
 
+        # Get alpha schedule for weighting boundary losses 
         self.alpha = AlphaSchedule(
             self.args.epochs,
             self.args.boundary_loss_schedule,
@@ -111,7 +112,7 @@ class Trainer:
         )
 
         # Get standard dice loss for validation
-        self.val_loss = DiceLoss()
+        self.val_loss = DiceCELoss()
 
         # Get VAE regularization loss
         self.vae_loss = VAELoss()
@@ -191,7 +192,7 @@ class Trainer:
                                                 rank=rank,
                                                 world_size=world_size)
 
-            # Get steps per epoch
+            # Get steps per epoch if not given by user
             if self.args.steps_per_epoch is None:
                 self.args.steps_per_epoch = len(train_images) // self.args.batch_size
             else:
@@ -223,7 +224,7 @@ class Trainer:
 
             # If using AMP, use gradient scaling
             if self.args.amp:
-                scaler = torch.cuda.amp.GradScaler()
+                scaler = torch.amp.GradScaler("cuda")
 
             # Only log metrics on first process (i.e., rank 0)
             if rank == 0:
@@ -245,18 +246,22 @@ class Trainer:
                 # Loss computation
                 def compute_loss():
                     output = model(image)
-
-                    if dtm is None:
-                        loss = loss_fn(label, output["prediction"])
-                    else:
+                    
+                    if self.args.use_dtms:
                         loss = loss_fn(label, output["prediction"], dtm, alpha)
+                    elif self.args.loss in ["cldice"]:
+                        loss = loss_fn(label, output["prediction"], alpha)
+                    else:
+                        loss = loss_fn(label, output["prediction"])
 
                     if self.args.deep_supervision:
                         for k, p in enumerate(output["deep_supervision"]):
-                            if dtm is None:
-                                loss += 0.5 ** (k + 1) * loss_fn(label, p)
-                            else:
+                            if self.args.use_dtms:
                                 loss += 0.5 ** (k + 1) * loss_fn(label, p, dtm, alpha)
+                            elif self.args.loss in ["cldice"]:
+                                loss += 0.5 ** (k + 1) * loss_fn(label, p, alpha)
+                            else:
+                                loss += 0.5 ** (k + 1) * loss_fn(label, p)
 
                         c_norm = 1 / (2 - 2 ** (-(len(output["deep_supervision"]) + 1)))
                         loss *= c_norm
@@ -332,13 +337,16 @@ class Trainer:
                     with TrainProgressBar(epoch + 1, fold, self.args.epochs, self.args.steps_per_epoch) as pb:
                         for i in range(self.args.steps_per_epoch):
                             data = train_loader.next()[0]
+                            alpha = self.alpha(epoch)
                             if self.args.use_dtms:
-                                alpha = self.alpha(epoch)
                                 image, label, dtm = data["image"], data["label"], data["dtm"]
                                 loss = train_step(image, label, dtm, alpha)
                             else:
                                 image, label = data["image"], data["label"]
-                                loss = train_step(image, label, None, None)
+                                if self.args.loss in ["cldice"]:
+                                    loss = train_step(image, label, None, alpha)
+                                else:
+                                    loss = train_step(image, label, None, None)
 
                             # Update lr schedule
                             scheduler.step()
@@ -353,13 +361,16 @@ class Trainer:
                 else:
                     for i in range(self.args.steps_per_epoch):
                         data = train_loader.next()[0]
+                        alpha = self.alpha(epoch)
                         if self.args.use_dtms:
-                            alpha = self.alpha(epoch)
                             image, label, dtm = data["image"], data["label"], data["dtm"]
                             loss = train_step(image, label, dtm, alpha)
                         else:
                             image, label = data["image"], data["label"]
-                            loss = train_step(image, label, None, None)
+                            if self.args.loss in ["cldice"]:
+                                loss = train_step(image, label, None, alpha)
+                            else:
+                                loss = train_step(image, label, None, None)
 
                         # Update lr schedule
                         scheduler.step()