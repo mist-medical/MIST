7a6563d7f083932acca4b6801a17661ea2a89a62
aecelaya <aecelaya@rice.edu>
2024-11-15 16:52:51 -0600

    Bug fix for training subsets of the folds.

--- mist/scripts/run_all_entrypoint.py
+++ mist/scripts/run_all_entrypoint.py
@@ -44,7 +44,7 @@ def run_all_entry():
     # Check if the number of folds is compatible with the number of folds
     # specified.
     if (
-        np.max(mist_arguments.folds) + 1 < mist_arguments.nfolds or
+        np.max(mist_arguments.folds) + 1 > mist_arguments.nfolds or
         len(mist_arguments.folds) > mist_arguments.nfolds
     ):
         raise AssertionError(
